8a1df4765fc55e18bbb7fec4ef1c1c3cac050098
sharanggupta <sharang.gupta@sitpune.edu.in>
2021-03-08 00:44:30 +0000

    Added new express routers

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 
 app = express();
+
+//MIDDLEWARES
+
+//custom middleware
+
+app.use(morgan('dev'));
+
+app.use((req, resp, next) => {
+  console.log('hello from the middleware!');
+  next();
+});
+
+app.use((req, resp, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 //middleware, it basically appends the post input in the body to the request object
 app.use(express.json());
@@ -11,19 +28,22 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//ROUTEHANDLERS
+
 //GET all tours
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
     },
   });
-});
+};
 
 //GET tour having id
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   //convert strings to numbers easily
   const id = req.params.id * 1;
 
@@ -33,10 +53,10 @@ app.get('/api/v1/tours/:id', (req, res) => {
   } else {
     res.status(200).json({ status: 'success', data: { tourMatched } });
   }
-});
+};
 
 //POST a new tour
-app.post('/api/v1/tours', (req, res) => {
+const addNewTour = (req, res) => {
   const id = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id }, req.body);
   tours.push(newTour);
@@ -54,10 +74,10 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
 //UPDATE a tour having tour id
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   const id = req.params.id * 1;
 
   if (!tours.find((tour) => tour.id === id)) {
@@ -83,10 +103,10 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       }
     );
   }
-});
+};
 
 //DELETE a tour having tour id
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   const id = req.params.id * 1;
 
   if (!tours.find((tour) => tour.id === id)) {
@@ -112,9 +132,53 @@ app.delete('/api/v1/tours/:id', (req, res) => {
       }
     );
   }
-});
+};
 
-const port = 3000;
+const getAllUsers = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'route not implemented yet' });
+};
+
+const getUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'route not implemented yet' });
+};
+
+const addNewUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'route not implemented yet' });
+};
+
+const updateUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'route not implemented yet' });
+};
+
+const deleteUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: 'error', message: 'route not implemented yet' });
+};
+
+//ROUTES
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter.route('/').get(getAllTours).post(addNewTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+app.use('/api/v1/tours', tourRouter);
+
+userRouter.route('/').get(getAllUsers).post(addNewUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+app.use('/api/v1/users', userRouter);
+
+//START SERVER
+const port = 8000;
 app.listen(port, () => {
   console.log(`running on port ${port}...`);
 });
